101e1e184f8ce75300f01b733ea240e15c2a7441
Dhirajlal Vishwakarma <47329713+dhiraj7894@users.noreply.github.com>
2020-12-22 16:37:49 +0530

    OKKKKKKKKKKK

---
+++ Assets/Script/Player Controller/dragGm_script.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dragGm_script : MonoBehaviour
+{
+    private Vector3 screenPoint;
+    private Vector3 offset;
+
+    void OnMouseDown()
+    {
+        screenPoint = Camera.main.WorldToScreenPoint(gameObject.transform.position);
+        //screenPoint = Camera.main.WorldToScreenPoint(new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, 0));
+
+        offset = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z));
+    }
+
+    void OnMouseDrag()
+    {
+        Vector3 curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenPoint.z);
+
+        Vector3 curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint) + offset;
+        transform.position = new Vector3(curPosition.x, curPosition.y, 0);
+        transform.position = new Vector3(curPosition.x - 0.3f, curPosition.y - 0.2f, 0);
+    }
+}
---
+++ Assets/Script/Player Controller/placingPlaceCheck.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class placingPlaceCheck : MonoBehaviour
+{
+    public GameObject hitChecker;
+
+
+
+    RaycastHit hit;
+    Ray ray;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RAYCAST();
+    }
+    void RAYCAST()
+    {
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit))
+        {
+            hitChecker.transform.position = hit.transform.position;
+        }
+    }
+}
